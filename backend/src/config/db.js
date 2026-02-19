86180b33f063e5bd625c6bd5650eb465d581990a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2026-02-20 03:30:35 +0600

    fix the BD url issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -24,7 +24,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false,
